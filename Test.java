27c3a884940560cd013eb61b162eda59f829828a
Simhachalam <93778241+msc6281@users.noreply.github.com>
2026-04-07 08:48:11 +0530

    Checking Run time exception review

--- Test.java
+++ Test.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
 public class Test {
      public static void main(String[] args) {
-        System.out.println("Welcome to AI Code Review");
+          Map<Integer,Integer>mp = new HashMap<>();
+           mp.put(0,1);
+           int xr = 0;
+           int cnt = 0;
+           for(int i:nums){
+               xr = xr^i;
+               int x = xr^k;
+               cnt += mp.get(x);
+               mp.put(xr,mp.get(xr)+1);
+           }
+        System.out.println(cnt);
      }
  }
